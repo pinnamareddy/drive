2bf2d90e518eff254d4ae749c5bb1349600f32a3
pinnamareddy <33382724+pinnamareddy@users.noreply.github.com>
2021-04-02 11:21:14 +0530

    Add files via upload

---
+++ google-drive-clone/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import Header from './components/header'
+import Sidebar from './components/sidebar'
+import FilesView from './components/filesView/FilesView'
+import SideIcons from './components/sideIcons'
+
+import GDriveLogo from './media/google-drive-logo.png'
+
+import { auth, provider } from "./firebase";
+import { useState } from 'react';
+
+function App() {
+  // const [user, setUser] = useState()
+  const [user, setUser] = useState()
+
+  const handleLogin = () => {
+    if (!user) {
+      auth.signInWithPopup(provider).then((result) => {
+        setUser(result.user)
+        })
+    } 
+  }
+  
+
+  return (
+    <div className="app">
+      {
+        user ? (
+          <>
+            <Header userPhoto={user.photoURL} />
+            <div className="app__main">
+              <Sidebar />
+              <FilesView />
+              <SideIcons />
+            </div>
+          </>
+        ) : (
+            <div className='app__login'>
+              <img src={GDriveLogo} alt="Google Drive" />
+              <button onClick={handleLogin}>Log in to Google Drive</button>
+            </div>
+          )
+      }
+    </div>
+  );
+}
+
+export default App;
